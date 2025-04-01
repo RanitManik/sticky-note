25ac2ea0f3e06d3c69c327984a28a614ddccf343
RanitManik <138437760+RanitManik@users.noreply.github.com>
2025-04-01 12:16:39 +0530

    update project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
-const App = () => {
-    return <div className="bg-red-200 text-sm md:bg-green-500">HOME</div>;
-};
+import { Block } from "@blocknote/core";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { useState } from "react";
 
-export default App;
+export default function App() {
+    // Creates a new editor instance.
+    const editor = useCreateBlockNote();
+
+    const [blocks, setBlocks] = useState<Block[]>([]);
+
+    // Renders the editor instance using a React component.
+    return (
+        <>
+            <BlockNoteView
+                editor={editor}
+                onChange={() => {
+                    // Saves the document JSON to state.
+                    setBlocks(editor.document);
+                }}
+            />
+        </>
+    );
+}
